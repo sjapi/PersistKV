5ff6e0fbb937867e58b67e7575d12606722f2e76
sjapi <aguznenkovv@yandex.ru>
2025-09-03 23:28:14 +0400

    Implement serialzer protocol & its implementation for int, double and std::string

---
+++ src/Serializer.h
@@ -0,0 +1,64 @@
+#ifndef __SERIALIZER_HPP__
+#define __SERIALIZER_HPP__
+
+#include <string>
+
+template <typename T>
+class PersistKVSerializer {
+public:
+	static std::string serialize(const T& obj) {
+		static_assert(sizeof(T) == 0, "Serializer not implemented for this type");
+		return {};
+	}
+
+	static T deserialize(const std::string& data) {
+		static_assert(sizeof(T) == 0, "Serializer not implemented for this type");
+		return T{};
+	}
+private:
+	PersistKVSerializer();
+};
+
+template <>
+class PersistKVSerializer<int> {
+public:
+	static std::string serialize(const int& obj) {
+		return std::to_string(obj);
+	}
+
+	static int deserialize(const std::string& data) {
+		return std::stoi(data);
+	}
+private:
+	PersistKVSerializer();
+};
+
+template <>
+class PersistKVSerializer<double> {
+public:
+	static std::string serialize(const double& obj) {
+		return std::to_string(obj);
+	}
+
+	static double deserialize(const std::string& data) {
+		return std::stod(data);
+	}
+private:
+	PersistKVSerializer();
+};
+
+template <>
+class PersistKVSerializer<std::string> {
+public:
+	static std::string serialize(const std::string& obj) {
+		return obj;
+	}
+
+	static std::string deserialize(const std::string& data) {
+		return data;
+	}
+private:
+	PersistKVSerializer();
+};
+
+#endif
---
+++ tests/serializer_test.cpp
@@ -0,0 +1,26 @@
+#include "Serializer.h"
+
+#include <string>
+#include <cassert>
+
+int main() {
+	/* Int */
+	int a1 = 42;
+	std::string s_a = PersistKVSerializer<int>::serialize(a1);
+	int a2 = PersistKVSerializer<int>::deserialize(s_a);
+	assert(a1 == a2);
+
+	/* Double */
+	double a3 = 3.14;
+	s_a = PersistKVSerializer<double>::serialize(a3);
+	double a4 = PersistKVSerializer<double>::deserialize(s_a);
+	assert(a3 == a4);
+
+	/* String */
+	std::string a5 = "NVIDIA's Nucleus team is the best";
+	s_a = PersistKVSerializer<std::string>::serialize(a5);
+	std::string a6 = PersistKVSerializer<std::string>::deserialize(s_a);
+	assert(a5 == a6);
+
+	return 0;
+}
